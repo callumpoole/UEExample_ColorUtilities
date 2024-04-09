8870a56f2900f8d52bab5c9ec329a4e10150534d
Callum Poole <14567682+callumpoole@users.noreply.github.com>
2024-04-09 22:52:23 +0100

    Inital Commit

---
+++ Source/ColorUtilsExample/ColorUtilsExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ColorUtilsExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COLORUTILSEXAMPLE_API AColorUtilsExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
